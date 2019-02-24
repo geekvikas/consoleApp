4350da61461c807482e0783fca6e20bbeb994538
Vikas Verma <vikas.verma@condecosoftware.com>
2019-02-24 03:07:16 -0800

    bug fixes to handle two players game play using command line

--- main.js
+++ main.js
@@ -2,13 +2,21 @@
 const t = require('./lib/ttt');
 var readlineSync = require('readline-sync');
 const ttt = new t();
-
+var vsCPU = true;
+var token = 'X';
 
 console.log(`Let's Play Tic-Tac-Toe`);
+if (readlineSync.question('Single Player (default) or Double Player (S/D): ').toUpperCase() === 'D')
+    vsCPU = false;
+
+if (readlineSync.question(`Select Player 1's Symbol (X/O): `).toUpperCase() === 'O')
+    token = 'O';
+
+ttt.StartGame(token,vsCPU);
 
 while (!ttt.GameOver()) {
     ttt.PrintInfoBoard();
-    ttt.MakeAMove(readlineSync.question('Your Position: '));
+    ttt.MakeAMove(readlineSync.question(`Player ${ttt.WhosNext()}, Your Move: `));
 }
 
 ttt.PrintBoard();
